a9bc6b7adb7649fffd2e9d00981a1f7c6c772288
Suyash-Kaul <suyashkaul28@gmail.com>
2026-08-05 16:35:41 +0530

    new frontend

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hi! I am a robot."
+    return render_template("index.html")
 
-@app.route("/health")
-def health():
-    return "I am working perfectly!"
+# @app.route("/health")
+# def health():
+#     return "I am working perfectly!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)   
